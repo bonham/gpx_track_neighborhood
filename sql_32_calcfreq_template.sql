44e68c069e28a0ca86f9465d9a86af5ca78df6b3
Art Furrer <artfurrer@elfu.de>
2018-01-02 23:24:08 +0100

    optimization with && operator

--- sql_32_calcfreq_template.sql
+++ sql_32_calcfreq_template.sql
@@ -2,11 +2,12 @@ insert into frequency(
     select 
         tp.ogc_fid, 
         tp.track_fid, 
-        sum(ST_NumGeometries(ST_Intersection(tr.wkb_geometry, ci.wkb_geometry ))),
+        sum(ST_NumGeometries(ST_Intersection(tr.wkb_geometry, ci.wkb_geometry ))) ,
         tp.wkb_geometry
     from tracksegments tr, circles ci, track_points tp 
     where 
-        tp.ogc_fid = ci.ogc_fid
+        tr.wkb_geometry && ci.wkb_geometry
+        and tp.ogc_fid = ci.ogc_fid
         and tp.track_fid = {}
     group by tp.track_fid, tp.ogc_fid
 )

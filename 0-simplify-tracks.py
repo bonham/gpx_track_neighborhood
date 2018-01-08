4884f2af9ddcc62a7d9df24978f26def9deffecb
Art Furrer <artfurrer@elfu.de>
2018-01-09 00:49:32 +0100

    README

---
+++ 0-simplify-tracks.py
@@ -0,0 +1,98 @@
+import sys
+import argparse
+import glob
+import os
+import subprocess
+import psycopg2
+
+if sys.version_info < (3,6):
+    raise RuntimeError("You must use python 3.6, You are using python {}.{}.{}".format(*sys.version_info[0:3]))
+
+# constants
+
+BABEL="gpsbabel"
+
+def main():
+
+    # pre check system environment
+    pre_check()
+
+    # parse args
+    (sourcedir, targetdir, error) = a_parse()
+
+    # get gpx filenames
+    gpx_filelist = getfiles(sourcedir)
+
+    print("Number of gpx files: {}".format(len(gpx_filelist)))
+
+    # run conversion
+    if not os.path.exists(targetdir):
+        print("Creating directory {}".format(targetdir))
+        os.makedirs(targetdir)
+
+    for sourcefile in gpx_filelist:
+
+        fname_base = os.path.basename(sourcefile)
+        targetfile = os.path.abspath(
+                os.path.join(targetdir, fname_base))
+
+        simplify_one(sourcefile, targetfile, error)
+    
+
+
+#--------------------------------
+def a_parse():
+    parser = argparse.ArgumentParser(
+            description = 
+                'Load GPX files in specified directory into postgis database'
+            )
+    parser.add_argument('source_directory')
+    parser.add_argument('target_directory')
+    parser.add_argument('--error',default='0.001k', help="Simplification factor. Higher values simplify more. It corresponds to xx in gpsbabel -x simplify,error=xx Default value is 0.001k")
+    args = parser.parse_args()
+
+    sd = os.path.abspath(args.source_directory)
+    td = os.path.abspath(args.target_directory)
+
+    if sd == td:
+        print("Source and target directory should not be the same")
+        sys.exit(1)
+    else:
+        return (sd, td, args.error)
+
+#--------------------------------
+def getfiles(directory):
+    
+    normdir = os.path.abspath(directory)
+    globexp = os.path.join(normdir, "*.gpx")
+    dirs = glob.glob(globexp)
+
+    return dirs
+
+def pre_check():
+
+    try:
+        subprocess.call((BABEL, "-?"), stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    except FileNotFoundError as e:
+        print("Error: The command {} could not be found on your system".format(BABEL))
+        sys.exit(1)
+
+def simplify_one(sourcefile, targetfile, error):
+
+    cmd = (
+            BABEL,
+            "-i", "gpx",
+            "-f", sourcefile,
+            "-x", "simplify,error={}".format(error),
+            "-o", "gpx",
+            "-F", targetfile
+            )
+
+    print("=== Simplifying from {} to {}".format(sourcefile, targetfile))
+    subprocess.check_call(cmd)
+
+
+##############################################
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- README.md
+++ README.md
@@ -1,23 +1,31 @@
-# gpx_track_neighborhood
+# GPX Proximator
 
-How close are my gpx tracks?
+* You have a set of gpx tracks
+* You want to display them on a map
+* You want color coding depending on how often you passed the same roads
 
-## Goal
-
-This project does calculate proximity of multiple tracks from gpx files
+The project does calculate proximity of multiple tracks from gpx files
 
 ## Howto
 
-* Set up postgis server on localhost
-* Run gpx2postgis.py <source_dir> <target_dbname>. The target db will be created or overwritten
-* Run frequency_calc.py
-* Result is in frequency table - each trackpoint has a counter on how many other tracks are nearby
+* Install dependencies from below
+Run all python commands below. Use --help to learn what arguments to provide
+* Run <strong>0-simplify-tracks.py</strong>. This will use gpsbabel to reduce gpx tracks.
+* Run <strong>1-gpx2postgis.py</strong> <source_dir> <target_dbname> to import gpx tracks to database. The database will be created or overwritten.
+* Run <strong>2-proximity-calc.py</strong> to calculate intersections of gpx tracks.
+* Run <strong>3-category-calc.py</strong> to calculate color categories.
+* Run <strong>4-export-geojson.py</strong> to create json files for html pages.
 
+The finished web pages are in subdirectory <strong>html</strong>. Push all files from that folder to a webserver and open it in browser. ( Firefox can open it also locally, but not Chrome. For Chrome you can start local webserver from html directory with <strong>startserver.bat</strong>)
+
+## Platforms
+
+Developed and tested on Windows. Will very likely run on linux too.
 ## Dependencies
 
-* python 3.6 or higher
-* psycopg2 python library for postgres access
-* gpsbabel
-* GDAL
-* posgtres with postgis extension
-* Tested on windows, but may run on linux too
+* Install postgres 10 from https://www.postgresql.org
+* Install postgis 2.4.2 from https://postgis.net
+* Install python 3.6 or higher ( python 2.7 may also work )
+* Install psycopg2 python library for postgres access
+* Install gpsbabel from https://www.gpsbabel.org/
+* Install GDAL from http://www.gdal.org/ ( You need ogr2ogr command line tool )

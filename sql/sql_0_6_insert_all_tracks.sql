a49c0056a6cfadd12b82b9899ddc5fdebe64fcb3
Art Furrer <artfurrer@elfu.de>
2019-01-06 17:07:24 +0100

    Fix possible mismatch in table columns

--- sql/sql_0_6_insert_all_tracks.sql
+++ sql/sql_0_6_insert_all_tracks.sql
@@ -1,6 +1,8 @@
-insert into all_tracks 
+insert into all_tracks ( track_id, file_id, ogc_fid, name, wkb_geometry )
 select 
     nextval('all_tracks_track_id_seq'),
     %s,
-    *
+    ogc_fid,
+    name,
+    wkb_geometry
 from tracks;

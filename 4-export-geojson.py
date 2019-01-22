60857b4aabb42dcf7a67536d1049c0120a44ec17
Art Furrer <artfurrer@elfu.de>
2019-01-22 23:12:50 +0100

    Create or extend datasets.json

--- 4-export-geojson.py
+++ 4-export-geojson.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import psycopg2
 import json
+import shutil
+import json
 
 if sys.version_info < (3,6):
     raise RuntimeError("You must use python 3.6, You are using python {}.{}.{}".format(*sys.version_info[0:3]))
@@ -12,6 +14,8 @@ if sys.version_info < (3,6):
 # constants
 
 PG_USER="postgres"
+METADATA_FNAME="datasets.json"
+BASEDIR=os.path.normpath("html/static/geojson")
 
 def main():
 
@@ -23,20 +27,20 @@ def main():
                 "dbname={} user={}".format(database_name, PG_USER))
     cur = conn.cursor()
 
-    geodir = os.path.normpath(
-            os.path.join(
-                "html/static/geojson",
-                outputDir)
-            )
-    oldfiles = glob.glob(os.path.join(geodir,'*'))
-    for remove_file in oldfiles:
-        print("Deleting {}".format(remove_file))
-        os.unlink(remove_file)
+    geodir = os.path.join(BASEDIR, outputDir)
 
+    # remove directory if exists
+    print("Removing {}".format(geodir))
+    if os.path.exists(geodir) and os.path.isdir(geodir):
+        shutil.rmtree(geodir)
+    
+    # recreate directory ( including parents )
+    os.makedirs(geodir, exist_ok=True)
 
+    # create or extend metadata file
+    create_extend_metadata(BASEDIR, METADATA_FNAME, outputDir)
 
     # read geojson
-    #cur.execute("select ST_AsGeoJSON(wkb_geometry) from tracks where tracks.ogc_fid = 2")
     cur.execute("select category, ST_AsGeoJSON(ST_Collect(wkb_geometry)), min(freq), max(freq) from track_segment_freq_categories group by category order by category")
     r = cur.fetchall()
 
@@ -53,7 +57,6 @@ def main():
         
         fname = os.path.join(geodir, "g_{}.json".format(i))
         print("Writing "+fname)
-        os.makedirs(geodir, exist_ok=True)
         with open(fname,"w") as f:
             f.write(js)
 
@@ -83,6 +86,33 @@ def a_parse():
 
     return (args.database, args.dataset_label)
 
+
+def create_extend_metadata(directory, filename, label):
+
+    fpath = os.path.join(directory, filename)
+    
+    # Create file if it is not existing
+    if os.path.exists(fpath):
+
+        with open(fpath, 'r') as fp:
+            mdata = json.load(fp)
+
+    else:
+
+        mdata = []
+
+
+    # append label if not in list
+    if label not in mdata:
+        print("Appending {} to {}".format(label, fpath))
+        mdata.append(label)
+    else:
+        print("Label {} is already in {}".format(label, fpath))
+
+    # write file
+    print("Writing {}".format(fpath))
+    with open(fpath, 'w') as fp:
+        json.dump(mdata, fp)
 
 
 ###################################################

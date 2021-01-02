d6f0a8b058b029b4694c3175a4addf0500e10b06
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-01-02 17:24:36 +0100

    intermediate work

--- gpx2db_pkg/gpx2db/utils.py
+++ gpx2db_pkg/gpx2db/utils.py
@@ -6,28 +6,51 @@ class SetupDb:
         self.tracks_id_sequence = "tracks_id"
         self.track_points_table = "track_points"
         self.track_points_id_sequence = "track_points_id"
-        self.track_segments_id_sequence = "track_segments_id"
 
         self.conn = database_connection
         self.cur = self.conn.cursor()
 
+    def drop_objects(self):
+
+        sequences = [
+            self.tracks_id_sequence,
+            self.track_points_id_sequence
+        ]
+        sequence_drop_commands = [
+            "drop sequence {}".format(i) for i in sequences]
+
+        tables = [
+            self.tracks_table,
+            self.track_points_table
+        ]
+        table_drop_commands = ["drop table {}".format(i) for i in tables]
+
+        for sql in (sequence_drop_commands + table_drop_commands):
+            try:
+                self.cur.execute(sql)
+            except Exception as e:
+                print(e)
+
     def commit(self):
         self.conn.commit()
 
-    def init_db(self):
+    def init_db(self, drop=False):
         "Create necessary sequences and tables"
+
+        if drop:
+            self.drop_objects()
+
         cur = self.cur
 
         self.create_sequence(self.tracks_id_sequence)
         self.create_sequence(self.track_points_id_sequence)
-        self.create_sequence(self.track_segments_id_sequence)
 
         sql_create_tracks_table = """
             create table {}
             (
             id integer PRIMARY KEY,
-            src_file varchar(1000),
-            description varchar(2000),
+            name varchar(2000),
+            src varchar(2000),
             wkb_geometry geometry(MultiLineString,4326)
             )
         """.format(
@@ -55,5 +78,63 @@ class SetupDb:
         sql = "CREATE SEQUENCE {}".format(sequence_name)
         self.cur.execute(sql)
 
-    def load_gpx_file(self, gpxpy_obj):
-        pass
+    def get_nextval(self, sequence):
+
+        sql = "select nextval({})".format(sequence)
+        self.cur.execute(sql)
+        row = self.cur.fetchone()
+        return row[0]
+
+    def load_gpx_file(self, gpx, src=None):
+
+        for track in gpx.tracks:
+
+            track_id = self.get_nextval(self.tracks_id_sequence)
+            self.store_track(track, track_id, src)
+
+            for segnum, segment in enumerate(track.segments):
+                print("Segment: {}".format(segnum))
+
+                for point in segment.points:
+                    point_id = self.get_nextval(self.track_points_id_sequence)
+                    self.store_point(
+                        point,
+                        point_id,
+                        track_id,
+                        segnum
+                    )
+
+        print("Committing ...")
+        self.commit()
+
+    def store_track(self, track, rowid, src=None):
+
+        name = track.name
+
+        sql = """
+            insert into {} (id, name, src)
+            values({},{},{}
+        """.format(
+            self.tracks_table,
+            rowid,
+            name,
+            src
+        )
+        self.cur.execute(sql)
+
+    def store_point(self, point, rowid, track_id, track_seg_id):
+
+        point_wkt = "ST_GeomFromText('POINT({} {})', 4326)".format(
+            point.longitude,
+            point.latitude
+        )
+
+        sql_template = """
+            insert into {} (id, track_id, track_segment_id, wkb_geometry)
+            values({},{},{},{})
+        """.format(
+            rowid,
+            track_id,
+            track_seg_id,
+            point_wkt
+        )
--- gpx2db_pkg/tests/test_gpx2db.py
+++ gpx2db_pkg/tests/test_gpx2db.py
@@ -1,17 +1,42 @@
 import pytest
 from mock import Mock
 from gpx2db import SetupDb
+import gpxpy
+from os import path
+import psycopg2 as pg2
+
+my_dir = path.dirname(__file__)
 
 
 @pytest.fixture
 def dbconn():
-    "Mocked connection"
-    return Mock()
+    mocking = False
+
+    if mocking:
+        return Mock()
+    else:
+        sysDBconn = pg2.connect(
+            "dbname={} host={} user={} password={} port={}".format(
+                "test3", "localhost", "postgres", "xxx", 5432
+            )
+        )
+        return sysDBconn
 
 
-@pytest.fixture
+@ pytest.fixture
 def gpxpy_obj():
-    return Mock()
+
+    def gpx_creator(i):
+        fname = path.join(
+            my_dir,
+            'test_data',
+            'file{}.gpx'.format(i)
+        )
+        gpx_file = open(fname, 'r')
+        gpx_o = gpxpy.parse(gpx_file)
+        return gpx_o
+
+    return gpx_creator
 
 
 class TestGpx2Db:
@@ -19,5 +44,5 @@ class TestGpx2Db:
     def test1(self, dbconn, gpxpy_obj):
 
         sd = SetupDb(dbconn)
-        sd.init_db()
-        sd.load_gpx_file(gpxpy_obj)
+        sd.init_db(drop=True)
+        sd.load_gpx_file(gpxpy_obj(1), src="file1")

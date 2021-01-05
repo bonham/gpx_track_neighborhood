b84b635244ce3da7eb71653b1fc7b2f8a9e521ae
Art Furrer <2823246+bonham@users.noreply.github.com>
2021-01-05 23:15:54 +0100

    cosmetics

--- sql/sql_32_calcfreq_template.sql
+++ sql/sql_32_calcfreq_template.sql
@@ -6,18 +6,18 @@ with dump as (
     select 
         tp.ogc_fid, 
         tp.track_id, 
-    	tr.segment_id as trackseg_id,
+    	se.segment_id as trackseg_id,
 		-- Split the multilinestrings
-    	(ST_Dump(ST_Multi(ST_Intersection(tr.wkb_geometry, ci.wkb_geometry )))).path[1] as path,
-        (ST_Dump(ST_Multi(ST_Intersection(tr.wkb_geometry, ci.wkb_geometry )))).geom as linestring,
+    	(ST_Dump(ST_Multi(ST_Intersection(se.wkb_geometry, ci.wkb_geometry )))).path[1] as path,
+        (ST_Dump(ST_Multi(ST_Intersection(se.wkb_geometry, ci.wkb_geometry )))).geom as linestring,
         tp.wkb_geometry
-    from newsegments tr, circles ci, newpoints tp 
+    from newsegments se, circles ci, newpoints tp 
     where 
-        tr.wkb_geometry && ci.wkb_geometry
+        se.wkb_geometry && ci.wkb_geometry
         and tp.ogc_fid = ci.ogc_fid
 		and tp.track_id = {}
 ) 
--- Now analyze all consecutive intersections for a given circle and tracksegment. If the lines do not touch we count it. 
+-- Now analyze all consecutive intersections for a given circle and tracksegment. If the lines do not touch we count them. 
 -- We also count the last intersection (=dump2.linestring is null because of left join and because dump2.path-1 does not exist because sequence is at its end)
 select
 	dump1.ogc_fid,
